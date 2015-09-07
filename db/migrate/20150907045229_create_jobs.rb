f542c9adda67a8628ca017b1a4ef633dd249198a
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-07 00:54:41 -0400

    initial commit

---
+++ db/migrate/20150907045229_create_jobs.rb
@@ -0,0 +1,10 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.text :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
