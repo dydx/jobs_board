e00e61627017ac5ee3a80bb98fbcec820beadb6c
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-09-08 23:25:11 -0400

    throwing this on github for safe keeping

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,18 @@ class JobsController < ApplicationController
   def index
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
 end
